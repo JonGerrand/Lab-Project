9a063d95c74f44f100ee96888e4ae7d35fb003ce
JonGerrand <gerrand.jonathan@gmail.com>
2015-07-28 14:14:04 +0200

    Added client functionality to continuously send messages

--- Client v_01.js
+++ Client v_01.js
@@ -1,11 +1,25 @@
 var net = require('net');
 
-var client = new net.Socket();
+// Code for writing from a multiple times
+//var readline = require('readline');
+// var rl = readline.createInterface(process.stdin, process.stdout);
+// rl.setPrompt('Please enter your message: ');var client = new net.Socket();
+
 client.setKeepAlive(true);
 
 client.connect(4040, '192.168.43.172', function(){
   console.log('Connected to remote Server');
   client.write('This is a message');
+
+  // rl.prompt();
+  // rl.on('line', function(line){
+  //   if (line === "close") rl.close();
+  //   client.write(line);
+  //   rl.prompt();
+  // }).on('close', function(){
+  //   process.exit(0);
+  // })
+
 });
 
 client.on('data', function(data){
