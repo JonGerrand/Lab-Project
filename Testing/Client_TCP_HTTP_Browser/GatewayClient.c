eda64026a6137f8ac4d2144cacc65c3e749be261
JonGerrand <gerrand.jonathan@gmail.com>
2015-09-08 11:49:17 +0200

    Validated TCP connection for C-based Clients   -Node will accept incoming TCP Sockets from any library

--- Testing/Client_TCP_HTTP_Browser/GatewayClient.c
+++ Testing/Client_TCP_HTTP_Browser/GatewayClient.c
@@ -1,49 +1,62 @@
-#include <stdio.h>
-#include <string.h>
-#include <errno.h>
-#include <sys/socket.h>
-#include <resolv.h>
+/*
+    C ECHO client example using sockets
+*/
+#include<stdio.h> //printf
+#include<string.h>    //strlen
+#include<sys/socket.h>    //socket
+#include<arpa/inet.h> //inet_addr
 
-#define PORT_TIME       13              /* "time" (not available on RedHat) */
-#define PORT_FTP        21              /* FTP connection port */
-#define SERVER_ADDR     "127.0.0.1"     /* localhost */
-#define MAXBUF          1024
+int main(int argc , char *argv[])
+{
+    int sock;
+    struct sockaddr_in server;
+    char message[1000] , server_reply[2000];
 
-int main()
-{   int sockfd;
-    struct sockaddr_in dest;
-    char buffer[MAXBUF];
-
-    /*---Open socket for streaming---*/
-    if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0 )
+    //Create socket
+    sock = socket(AF_INET , SOCK_STREAM , 0);
+    if (sock == -1)
     {
-        perror("Socket");
-        exit(errno);
+        printf("Could not create socket");
+    }
+    puts("Socket created");
+
+    server.sin_addr.s_addr = inet_addr("192.168.43.63");
+    server.sin_family = AF_INET;
+    server.sin_port = htons( 4040 );
+
+    //Connect to remote server
+    if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0)
+    {
+        perror("connect failed. Error");
+        return 1;
     }
 
-    /*---Initialize server address/port struct---*/
-    bzero(&dest, sizeof(dest));
-    dest.sin_family = AF_INET;
-    dest.sin_port = htons(PORT_FTP);
-    if ( inet_aton(SERVER_ADDR, &dest.sin_addr.s_addr) == 0 )
+    puts("Connected\n");
+
+    //keep communicating with server
+    while(1)
     {
-        perror(SERVER_ADDR);
-        exit(errno);
+        printf("Enter message : ");
+        scanf("%s" , message);
+
+        //Send some data
+        if( send(sock , message , strlen(message) , 0) < 0)
+        {
+            puts("Send failed");
+            return 1;
+        }
+
+        //Receive a reply from the server
+        if( recv(sock , server_reply , 2000 , 0) < 0)
+        {
+            puts("recv failed");
+            break;
+        }
+
+        puts("Server reply :");
+        puts(server_reply);
     }
 
-    /*---Connect to server---*/
-    if ( connect(sockfd, (struct sockaddr*)&dest, sizeof(dest)) != 0 )
-    {
-        perror("Connect ");
-        exit(errno);
-    }
-
-    /*---Get "Hello?"---*/
-    bzero(buffer, MAXBUF);
-    recv(sockfd, buffer, sizeof(buffer), 0);
-    printf("%s", buffer);
-
-    /*---Clean up---*/
-    close(sockfd);
+    close(sock);
     return 0;
 }
