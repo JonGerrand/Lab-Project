95da5de5e1144ac5ae253c70e0dc7e69db18bfd9
JonGerrand <gerrand.jonathan@gmail.com>
2015-09-16 11:03:32 +0200

    Added Winston and completed Testing
    -Added Winston.js as a potential data logger
    -Cleared testing lines of code

--- Testing/DVM/app.js
+++ Testing/DVM/app.js
@@ -17,7 +17,7 @@ var tcpSock = require('net');
 
 //SARM connection configuration
 var tcp_PORT = 4040;
-var tcp_HOST = '192.168.43.192';
+var tcp_HOST = '192.168.1.3';
 
 //Routing Config for express
 var routes = require('./routes/index');
@@ -44,17 +44,6 @@ var convertMsToTimestamp = function(msString){
 };
 //----------------------------------
 
-// Testing variables
-var pi_old = 0;
-var gateway_old = 0;
-var sarm_old = 0;
-var dvm_old = 0;
-var pi_new = 0;
-var gateway_new = 0;
-var sarm_new = 0;
-var dvm_new = 0;
-var dataCount = 0;
-
 //----------Socket.io-------------------
 var webSock = socket_io();
 app.io = webSock;
@@ -75,25 +64,10 @@ webSock.sockets.on("connection", function(socket){
     socket.emit("httpServer_msg", "Connected to HTTP Server");
     // Emit data to Websockets
     tcpClient.on('data', function(data){
-      console.log("Received Data");
-      // Delta calculations
-      var timeStamps = data.split(",")
-      pi_old = pi_new;
-      gateway_old = gateway_new;
-      sarm_old = sarm_new;
-      dvm_old = dvm_new;
-      dvm_new = new Date();
-      pi_new = new Date(parseFloat(timeStamps[1]));
-      gateway_new = new Date(parseFloat(timeStamps[2]));
-      sarm_new = new Date(parseFloat(timeStamps[3]));
-      var delta_pi = pi_new - pi_old;
-      var delta_gateway = gateway_new - gateway_old;
-      var delta_sarm = sarm_new - sarm_old;
-      var delta_dvm = dvm_new - dvm_old;
-      console.log("Pi: " + delta_pi + " Gate: " + delta_gateway + " SARM: " + delta_sarm + " DVM: " + delta_dvm);
-      socket.emit("httpServer_ord", data + ", DVM: " + delta_dvm);
+      console.log("Received Data: " + data);
+      //socket.emit("httpServer_ord", data + ", DVM: " + delta_dvm);
       // socket.emit("httpServer_alert", data);
-      socket.emit("httpServer_msg",  data + ", DVM: " + delta_dvm)
+      socket.emit("httpServer_msg", "\n" + "Pi: " + delta_pi);
       });
     // Web socket closed
     socket.on('disconnect',function(){
